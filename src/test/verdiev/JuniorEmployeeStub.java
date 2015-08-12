cbdd4d08d96bb7a1355437c86ed3b5b69075ce82
yioteh <rtoor.3io7@gmail.com>
2015-08-12 19:27:12 +0300

    junior stub

---
+++ src/test/verdiev/JuniorEmployeeStub.java
@@ -0,0 +1,17 @@
+package test.verdiev;
+
+/**
+ * Created by yioteh on 11.08.15.
+ */
+public class JuniorEmployeeStub {
+
+    public JuniorEmployeeStub(){}
+
+    public int getHours(){
+        return 34;
+    }
+
+    public int getAge(){
+        return 20;
+    }
+}
