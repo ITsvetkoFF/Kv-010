a4e4800388f786b890d6a664ead8253c026733c8
VStako <folk89@rambler.ru>
2015-08-12 17:36:36 +0300

    to master

---
+++ src/test/IJuniorEmployee.java
@@ -0,0 +1,9 @@
+package test;
+
+/**
+ * Created by stako on 11.08.2015.
+ */
+public interface IJuniorEmployee {
+    double getPayment();
+    public String toString();
+}
---
+++ src/test/TestEmployeeThrowMyException.java
@@ -0,0 +1,17 @@
+package test;
+
+import com.softserve.edu.statsenko.MyException;
+import org.junit.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by stako on 11.08.2015.
+ */
+public class TestEmployeeThrowMyException {
+
+    @Test(expectedExceptions = MyException.class, timeOut = 10000)
+    public void testEmployeeThrowMyException(){
+        StubEmployee employee = new StubEmployee();
+        Assert.assertNotNull(employee);
+    }
+}
