60bed2290438c99c49830e6868b71a30b5c6a3e1
Vadym Shevchenko <spawnkiev@gmail.com>
2015-08-25 18:37:12 +0300

    refactoring ResourcesPage

--- src/test/java/com/saucelabs/ResourcesPage.java
+++ src/test/java/com/saucelabs/ResourcesPage.java
@@ -3,20 +3,16 @@ package com.saucelabs;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import utility.Constant;
-import utility.ExcelUtils;
+import org.openqa.selenium.support.ui.Select;
 
-import java.net.*;
-import java.nio.charset.Charset;
 import java.util.List;
 
 /**
  * Created by Olya on 10/26/14.
  * Refactoring by Vadym on 08/25/15.
  */
-public class ResourcesPage extends AnyPage implements IResourcesPage{
+public class ResourcesPage extends AnyPage implements IResourcesPage, IAnyPage, IMapPage {
+
     public static final By RESOURCE_BUTTON = By.linkText("РЕСУРСИ");
     public static final By ADD_NEW_RESOURCE_BUTTON = By.linkText("ДОДАТИ НОВИЙ РЕСУРС");
     public static final By RESOURCE_TITLE = By.name("Title");
@@ -39,40 +35,47 @@ public class ResourcesPage extends AnyPage implements IResourcesPage{
         this.driver = driver;
     }
 
-    public void createResource(String sTitle, String sAlias, String sBody, String sPlaceToSave) throws Exception{
-
+    /**
+     * This method creates new resource on the website.
+     *
+     * @param sTitle The name of the new resource; may not be null.
+     * @param sAlias The alias of the new resource; may not be null.
+     * @param sBody The description of the new resource; may not be null.
+     * @param sPlaceToSave Place to save.
+     */
+    public void createResource(String sTitle, String sAlias, String sBody, String sPlaceToSave)  {
         driver.findElement(RESOURCE_BUTTON).click();
         driver.findElement(ADD_NEW_RESOURCE_BUTTON).click();
         driver.findElement(RESOURCE_TITLE).sendKeys(sTitle);
         driver.findElement(RESOURCE_ALIAS).sendKeys(sAlias);
         driver.findElement(RESOURCE_BODY).sendKeys(sBody);
-        org.openqa.selenium.support.ui.Select select = new org.openqa.selenium.support.ui.Select(driver.findElement(RESOURCE_SAVE_PLACE));
+        Select select = new Select(driver.findElement(RESOURCE_SAVE_PLACE));
         select.selectByVisibleText(sPlaceToSave);
         driver.findElement(SEND_BUTTON).click();
         driver.findElement(LOGO).click();
-
     }
 
-    public String existResource(String resource) throws  Exception{
-        String value = null;
+    /**
+     * Method searches where the resources are located.
+     *
+     * @param resource name of the resource.
+     * @return text where resource is located (top menu or in Resources).
+     */
+    public String existResource(String resource) {
+        String value = "";
         driver.findElement(LOGO).click();
-        //try {
-        //WebDriverWait wait = new WebDriverWait(driver, 3);
-        //WebElement elements = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".b-menu__button.ng-scope")));
         List<WebElement> resources1 = driver.findElements(HEADER_RESOURCES);
-        for (WebElement listElement : resources1){
+        for (WebElement listElement : resources1) {
             String searchText = listElement.getText();
-            if (searchText.equals(resource.toUpperCase())){
+            if (searchText.equalsIgnoreCase(resource)) {
                 value = "У верхньому меню";
             }
         }
-        //}
-        //catch (Exception e) {};
         driver.findElement(RESOURCE_BUTTON).click();
         List<WebElement> resources2 = driver.findElements(LIST_RESOURCES);
-        for (WebElement listElement : resources2){
-            //System.out.println(listElement.getText());
-            if (listElement.getText().equals(resource)){
+        for (WebElement listElement : resources2) {
+            String searchText = listElement.getText();
+            if (searchText.equalsIgnoreCase(resource)) {
                 value = "В розділі \"Ресурси\"";
             }
         }
@@ -80,12 +83,17 @@ public class ResourcesPage extends AnyPage implements IResourcesPage{
         return value;
     }
 
-    public void editResourceHeader(String sTitle, String sTextToAdd) throws Exception{
-
+    /**
+     * This method allows you to edit the header of the resource.
+     *
+     * @param sTitle The name of the resource; may not be null.
+     * @param sTextToAdd The new resource title; may not be null.
+     */
+    public void editResourceHeader(String sTitle, String sTextToAdd) {
         List<WebElement> resources = driver.findElements(HEADER_RESOURCES);
-        for (WebElement listElement : resources){
+        for (WebElement listElement : resources) {
             String searchText = listElement.getText();
-            if (searchText.equals(sTitle.toUpperCase())){
+            if (searchText.equalsIgnoreCase(sTitle)) {
                 listElement.findElement(EDIT_HEADER_RESOURCES).click();
                 driver.findElement(RESOURCE_TITLE).sendKeys(sTextToAdd);
                 driver.findElement(SEND_BUTTON).click();
@@ -95,12 +103,17 @@ public class ResourcesPage extends AnyPage implements IResourcesPage{
         driver.findElement(LOGO).click();
     }
 
-    public void editResourceList(String sTitle, String sTextToAdd) throws  Exception{
-
+    /**
+     * This method allows you to edit the list (drop-down menu) of the resources.
+     *
+     * @param sTitle The name of the resource; may not be null.
+     * @param sTextToAdd The new resource title; may not be null.
+     */
+    public void editResourceList(String sTitle, String sTextToAdd) {
         driver.findElement(RESOURCE_BUTTON).click();
         List<WebElement> resources = driver.findElements(LIST_RESOURCES);
-        for (WebElement listElement : resources){
-            if (listElement.getText().equals(sTitle)){
+        for (WebElement listElement : resources) {
+            if (listElement.getText().equals(sTitle)) {
                 listElement.findElement(EDIT_LIST_RESOURCES).click();
                 driver.findElement(RESOURCE_TITLE).sendKeys(sTextToAdd);
                 driver.findElement(SEND_BUTTON).click();
@@ -110,25 +123,32 @@ public class ResourcesPage extends AnyPage implements IResourcesPage{
         driver.findElement(LOGO).click();
     }
 
-    public void editResource(String sTitle, String sTextToAdd) throws  Exception{
+    /**
+     * This method allows you to edit the resource depends where it located (top menu or in Resources).
+     *
+     * @param sTitle The name of the resource; may not be null.
+     * @param sTextToAdd The resource title; may not be null.
+     */
+    public void editResource(String sTitle, String sTextToAdd) {
         String place = existResource(sTitle);
-        if (place.equals("В розділі \"Ресурси\"")){
+        if (place.equals("В розділі \"Ресурси\"")) {
             editResourceList(sTitle, sTextToAdd);
-        }
-        else if (place.equals("У верхньому меню")){
+        } else if (place.equals("У верхньому меню")) {
             editResourceHeader(sTitle, sTextToAdd);
         }
     }
 
-    public void deleteResourceFromHeader(String deleteTitle) throws Exception{
-
+    /**
+     * This method allows you to delete resource from header.
+     * @param deleteTitle Which resource need to delete.
+     */
+    public void deleteResourceFromHeader(String deleteTitle) {
         driver.findElement(LOGO).click();
         List<WebElement> resources = driver.findElements(HEADER_RESOURCES);
-        for (WebElement listElement : resources){
+        for (WebElement listElement : resources) {
             String searchText = listElement.getText();
-            if (searchText.equals(deleteTitle.toUpperCase())){
+            if (searchText.equalsIgnoreCase(deleteTitle)) {
                 listElement.findElement(DELETE_HEADER_RESOURCES).click();
-                //driver.findElement(By.partialLinkText("Видалити ресурс")).click();
                 driver.findElement(SUBMIT_DELITING).click();
                 break;
             }
@@ -137,27 +157,32 @@ public class ResourcesPage extends AnyPage implements IResourcesPage{
 
     }
 
-    public void deleteResourceFromList(String deleteTitle) throws Exception{
-
+    /**
+     * This method allows you to delete resource from list (drop-down menu).
+     * @param deleteTitle Which resource need to delete.
+     */
+    public void deleteResourceFromList(String deleteTitle) {
         driver.findElement(LOGO).click();
         driver.findElement(RESOURCE_BUTTON).click();
         List<WebElement> resources = driver.findElements(LIST_RESOURCES);
-        for (WebElement listElement : resources){
-            if (listElement.getText().equals(deleteTitle)){
+        for (WebElement listElement : resources) {
+            if (listElement.getText().equals(deleteTitle)) {
                 listElement.findElement(DELETE_LIST_RESOURCES).click();
-                //driver.findElement(By.partialLinkText("Видалити ресурс")).click();
                 driver.findElement(SUBMIT_DELITING).click();
                 break;
             }
         }
     }
 
-    public void deleteResource(String deleteTitle) throws  Exception{
+    /**
+     * This method find where resource is located and choose correct next method for this operation
+     * @param deleteTitle Which resource need to delete.
+     */
+    public void deleteResource(String deleteTitle) {
         String place = existResource(deleteTitle);
-        if (place.equals("В розділі \"Ресурси\"")){
+        if (place.equals("В розділі \"Ресурси\"")) {
             deleteResourceFromList(deleteTitle);
-        }
-        else if (place.equals("У верхньому меню")){
+        } else if (place.equals("У верхньому меню")) {
             deleteResourceFromHeader(deleteTitle);
         }
     }
