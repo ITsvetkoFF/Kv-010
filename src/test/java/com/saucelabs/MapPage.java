52d9163c7c3b99db6c766c3251b5216536c0f139
Vadym Shevchenko <spawnkiev@gmail.com>
2015-08-25 11:26:12 +0300

    refactor some methods in MapPage and added java doc comments_3

--- src/test/java/com/saucelabs/MapPage.java
+++ src/test/java/com/saucelabs/MapPage.java
@@ -35,14 +35,23 @@ public class MapPage implements IMapPage {
         this.driver = driver;
     }
 
+    /**
+     * With this method we can change view our page, we use coordinate and zoom for it
+     *
+     * @param latitude is coordinate in our map page (default latitude when you open the main page is 50)
+     * @param longitude is coordinate in our map page (default latitude when you open the main page is 32)
+     * @param zoom is in and out map (default 7 'full view of the map of Ukraine')
+     */
     @Override
     public void setView(double latitude, double longitude, int zoom) {
         JavascriptExecutor script = null;
         if (driver instanceof JavascriptExecutor)
             script = (JavascriptExecutor) driver;
-        script.executeScript("var map = document.getElementById(\"map-content\");" +
-                "angular.element(map).scope().$parent.$parent.$parent.geoJson._map.setView(["
-                + latitude + "," + longitude + "]" + "," + zoom + ");");
+        if (script != null) {
+            script.executeScript("var map = document.getElementById(\"map-content\");" +
+                    "angular.element(map).scope().$parent.$parent.$parent.geoJson._map.setView(["
+                    + latitude + "," + longitude + "]" + "," + zoom + ");");
+        }
     }
 
     /**
@@ -50,9 +59,10 @@ public class MapPage implements IMapPage {
      * 1) menu 'addProblem' can be at right side if your screen is normal
      * 2) menu 'addProblem' can be at top side if you have a small size screen
      * After this operations we would found new center coordinate X, Y. Run angular script which uses our variables
-     * @param latitude
-     * @param longitude
-     * @param zoom
+     *
+     * @param latitude is coordinate in our map page (default latitude when you open the main page is 50)
+     * @param longitude is coordinate in our map page (default latitude when you open the main page is 32)
+     * @param zoom is in and out map (default 7 'full view of the map of Ukraine')
      */
     @Override
     public void setVisibleView(double latitude, double longitude, int zoom) {
@@ -80,10 +90,14 @@ public class MapPage implements IMapPage {
         driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
 
         System.out.println("Before Explicit Wait during set view");
-        WebDriverWait webDriverWait = new WebDriverWait(driver, 1);
-        WebElement addProblem = webDriverWait.until(ExpectedConditions.presenceOfElementLocated(ADD_PROBLEM_MENU));
-        addProblemWidth = addProblem.getSize().getWidth();
-        addProblemHeight = addProblem.getSize().getHeight();
+        try {
+            WebDriverWait webDriverWait = new WebDriverWait(driver, 1);
+            WebElement addProblem = webDriverWait.until(ExpectedConditions.presenceOfElementLocated(ADD_PROBLEM_MENU));
+            addProblemWidth = addProblem.getSize().getWidth();
+            addProblemHeight = addProblem.getSize().getHeight();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         System.out.println("After Explicit Wait during set view");
 
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
@@ -92,7 +106,8 @@ public class MapPage implements IMapPage {
 
         if (mapWidth == addProblemWidth) {              // tall screen, menu 'addProblem' at the top
             newCenterCoordinateX = mapWidth / 2;
-            newCenterCoordinateY = navBarHeight + 1 + addProblemHeight + 1 + (mapHeight - navBarHeight - addProblemHeight - 2) / 2;
+            newCenterCoordinateY = navBarHeight + 1 + addProblemHeight + 1 +
+                                  (mapHeight - navBarHeight - addProblemHeight - 2) / 2;
         } else {                                        // wide screen, menu 'addProblem' at the right side
             newCenterCoordinateX = (mapWidth - addProblemWidth) / 2;
             newCenterCoordinateY = mapHeight / 2;
@@ -120,23 +135,16 @@ public class MapPage implements IMapPage {
         }
     }
 
-    @Override
-    public void clickAtPagesCenter() {
-        int x;
-        int y;
-
-        WebElement map = driver.findElement(MAP);
-        Dimension point = map.getSize();
-        x = point.getWidth() / 2;
-        y = point.getHeight() / 2;
-        Actions builder = new Actions(driver);
-        builder.moveToElement(map, x, y).clickAndHold().release().build().perform();
-    }
-
+    /**
+     * This method clicks on the center visible map. Finding menu 'addProblem' and on depends where this menu located
+     * click on the center. Offset helps shift your visible map on int distance
+     *
+     * @param offset is shift of the coordinate newCenterCoordinateY
+     */
     @Override
     public void clickAtVisibleMapCenter(int offset) {
-        int x;
-        int y;
+        int newCenterCoordinateX;
+        int newCenterCoordinateY;
         int mapWidth;
         int mapHeight;
         int addProblemWidth = 0;
@@ -145,129 +153,132 @@ public class MapPage implements IMapPage {
         WebElement map = driver.findElement(MAP);
         int navBarHeight = driver.findElement(NAV_BAR).getSize().getHeight();
         driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+
         System.out.println("Before Explicit Wait during click");
         try {
-            WebElement addProblem = (new WebDriverWait(driver, 1))
-                    .until(ExpectedConditions.presenceOfElementLocated(ADD_PROBLEM_MENU));
+            WebDriverWait webDriverWait = new WebDriverWait(driver, 1);
+            WebElement addProblem = webDriverWait.until(ExpectedConditions.presenceOfElementLocated(ADD_PROBLEM_MENU));
             addProblemWidth = addProblem.getSize().getWidth();
             addProblemHeight = addProblem.getSize().getHeight();
         } catch (Exception e) {
+            e.printStackTrace();
         }
         System.out.println("After Explicit Wait during click");
+
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
         mapWidth = map.getSize().getWidth();
         mapHeight = map.getSize().getHeight();
-        if (mapWidth != addProblemWidth) {              // wide screen, addProblem at the left side
-            x = (mapWidth - addProblemWidth) / 2;
-            y = mapHeight / 2;
-        } else {                                          // tall screen, addProblem at the top
-            x = mapWidth / 2;
-            y = navBarHeight + 1 + addProblemHeight + 1 + (mapHeight - navBarHeight - addProblemHeight - 2) / 2;
+        if (mapWidth == addProblemWidth) {              // tall screen, addProblem at the top
+            newCenterCoordinateX = mapWidth / 2;
+            newCenterCoordinateY = navBarHeight + 1 + addProblemHeight + 1 +
+                    (mapHeight - navBarHeight - addProblemHeight - 2) / 2;
+
+        } else {                                        // wide screen, addProblem at the right side
+            newCenterCoordinateX = (mapWidth - addProblemWidth) / 2;
+            newCenterCoordinateY = mapHeight / 2;
         }
         Actions builder = new Actions(driver);
-        builder.moveToElement(map, x, y + offset).clickAndHold().release().build().perform();
+        builder.moveToElement(map, newCenterCoordinateX, newCenterCoordinateY + offset)
+                             .clickAndHold().release().build().perform();
+    }
+
+    /**
+     * This method just click at the center of page.
+     */
+    @Override
+    public void clickAtPagesCenter() {
+        int centerCoordinateX;
+        int centerCoordinateY;
+
+        WebElement map = driver.findElement(MAP);
+        Dimension point = map.getSize();
+        centerCoordinateX = point.getWidth() / 2;
+        centerCoordinateY = point.getHeight() / 2;
+        Actions builder = new Actions(driver);
+        builder.moveToElement(map, centerCoordinateX, centerCoordinateY).clickAndHold().release().build().perform();
     }
 
     @Override
     public void clickAtProblemByCoordinateVisible(double latitude, double longitude) {
-
         setVisibleView(latitude, longitude, 18);
         clickAtVisibleMapCenter(-10);
     }
 
+    /**
+     * @param typeNumber is id for finding element in the List.
+     * @return text problem type from List.
+     */
     @Override
     public String getFilterTitle(int typeNumber) {
-
         List<WebElement> names = driver.findElements(PROBLEM_TYPE);
-
         return names.get(typeNumber).getAttribute("textContent");
     }
 
+    /**
+     * Zoom out to a maximum when the button 'zoom-out' would disabled.
+     */
     @Override
     public void clickZoomOut() {
         WebElement zoomOut = driver.findElement(ZOOM_OUT);
-
-        do {
+        while (!zoomOut.getAttribute("class").contains("disabled")){
             zoomOut.click();
-        } while (!zoomOut.getAttribute("class").contains("disabled"));
+        }
     }
 
+    /**
+     * Open filter menu.
+     */
     @Override
     public void openFiltersBoard() {
         driver.findElement(LEFT_SIDE_POINTER).click();
     }
 
-    @Override
-    public void selectAllExceptOneFilter(int typeNumber) throws Exception {
-
-        List<WebElement> filtersNames = driver.findElements(BY_PROBLEM_TYPE_WITH_LABEL_FOR);
-        List<WebElement> filtersChecks = driver.findElements(PROBLEM_TYPE_STARTS_WITH_TYPE_BY_ID);
-        String typeId = "";
-//        JavascriptExecutor js = null;
-//        if (driver instanceof JavascriptExecutor) {
-//            js = (JavascriptExecutor) driver;
-//        }
-
-        for (int i = 0; i < filtersChecks.size(); i++) {
-            typeId = filtersChecks.get(i).getAttribute("id");
-            if (i != typeNumber - 1) {
-                if (!"true".equals(filtersChecks.get(i).getAttribute("checked"))) filtersNames.get(i).click();
-//                js.executeScript("document.getElementById('" + typeId + "').checked = true");
-            } else {
-                if ("true".equals(filtersChecks.get(i).getAttribute("checked"))) filtersNames.get(i).click();
-//                js.executeScript("document.getElementById('" + typeId + "').checked = false");
-            }
-        }
-    }
-
-    @Override
-    public void selectAllExceptOneFilter(String typeName) {
-
-        List<WebElement> filtersNames = driver.findElements(BY_PROBLEM_TYPE_WITH_LABEL_FOR);
-        List<WebElement> filtersChecks = driver.findElements(PROBLEM_TYPE_STARTS_WITH_TYPE_BY_ID);
-        String typeId = "";
-        for (int i = 0; i < filtersChecks.size(); i++) {
-            typeId = filtersChecks.get(i).getAttribute("id");
-            if (!typeName.equals(filtersNames.get(i).getText())) {
-                if ("true".equals(filtersChecks.get(i).getAttribute("checked"))) filtersNames.get(i).click();
-            } else {
-                if (!"true".equals(filtersChecks.get(i).getAttribute("checked"))) filtersNames.get(i).click();
-            }
-        }
-    }
-
+    /**
+     * Find all problems with one filter by id (int)
+     * @param typeNumber is filter (id) for searching problems.
+     */
     @Override
     public void selectOnlyOneFilter(int typeNumber) {
-
         List<WebElement> filtersNames = driver.findElements(BY_PROBLEM_TYPE_WITH_LABEL_FOR);
         List<WebElement> filtersChecks = driver.findElements(PROBLEM_TYPE_STARTS_WITH_TYPE_BY_ID);
-        String typeId = "";
         for (int i = 0; i < filtersChecks.size(); i++) {
-            typeId = filtersChecks.get(i).getAttribute("id");
             if (i != typeNumber - 1) {
-                if ("true".equals(filtersChecks.get(i).getAttribute("checked"))) filtersNames.get(i).click();
+                if ("true".equals(filtersChecks.get(i).getAttribute("checked"))){
+                    filtersNames.get(i).click();
+                }
             } else {
-                if (!"true".equals(filtersChecks.get(i).getAttribute("checked"))) filtersNames.get(i).click();
+                if (!"true".equals(filtersChecks.get(i).getAttribute("checked"))) {
+                    filtersNames.get(i).click();
+                }
             }
         }
     }
 
+    /**
+     * Find all problems with one filter by text (string)
+     * @param typeName is filter (String) for searching problems.
+     */
     @Override
     public void selectOnlyOneFilter(String typeName) {
-
         List<WebElement> filtersNames = driver.findElements(BY_PROBLEM_TYPE_WITH_LABEL_FOR);
         List<WebElement> filtersChecks = driver.findElements(PROBLEM_TYPE_STARTS_WITH_TYPE_BY_ID);
-        String typeId = "";
         for (int i = 0; i < filtersChecks.size(); i++) {
-            typeId = filtersChecks.get(i).getAttribute("id");
             if (!typeName.equals(filtersNames.get(i).getText())) {
-                if ("true".equals(filtersChecks.get(i).getAttribute("checked"))) filtersNames.get(i).click();
+                if ("true".equals(filtersChecks.get(i).getAttribute("checked"))) {
+                    filtersNames.get(i).click();
+                }
             } else {
-                if (!"true".equals(filtersChecks.get(i).getAttribute("checked"))) filtersNames.get(i).click();
+                if (!"true".equals(filtersChecks.get(i).getAttribute("checked"))) {
+                    filtersNames.get(i).click();
+                }
             }
         }
     }
 
+    /**
+     * This method finds elements filtering by data. Get first element in the list and sets value.
+     * @param afterDate is parameter for set value in WebElement.
+     */
     @Override
     public void setAfterDate(String afterDate) {
         List<WebElement> dateFields = driver.findElements(INPUT_FIELD_FOR_DATE);
@@ -276,6 +287,10 @@ public class MapPage implements IMapPage {
         dateField.sendKeys(afterDate);
     }
 
+    /**
+     * This method finds elements filtering by data. Get second element in the list and sets value.
+     * @param beforeDate is parameter for set value in WebElement.
+     */
     @Override
     public void setBeforeDate(String beforeDate) {
         List<WebElement> dateFields = driver.findElements(INPUT_FIELD_FOR_DATE);
@@ -284,9 +299,12 @@ public class MapPage implements IMapPage {
         dateField.sendKeys(beforeDate);
     }
 
+    /**
+     * This method clicks on the buttons in filter menu (Filter by date). Just open calendar in filter menu and
+     * press buttons today, clear, close etc.
+     */
     @Override
     public void datePickersButtons() {
-
         List<WebElement> datePickerButtons = driver.findElements(DATE_PICKER);
 
         for (WebElement datePickers : datePickerButtons) {
@@ -308,25 +326,9 @@ public class MapPage implements IMapPage {
 
     @Override //not use
     public void selectDate(WebElement datePicker, String year, String month, String day) {
-
-        StringBuilder yearXPath = new StringBuilder("");
-        yearXPath.append(".//td/button/span[(text()='");
-        yearXPath.append(year);
-        yearXPath.append("')]");
-
-        StringBuilder monthXPath = new StringBuilder("");
-        monthXPath.append(".//td/button/span[(text()='");
-        monthXPath.append(month);
-        monthXPath.append("')]");
-
-        StringBuilder dayXPath = new StringBuilder("");
-        dayXPath.append(".//td/button/span[(text()='");
-        dayXPath.append(day);
-        dayXPath.append("')]/..");
-
-        datePicker.findElement(By.xpath(yearXPath.toString())).click();
-        datePicker.findElement(By.xpath(monthXPath.toString())).click();
-        datePicker.findElement(By.xpath(dayXPath.toString())).click();
+        datePicker.findElement(By.xpath(".//td/button/span[(text()='" + year + "')]")).click();
+        datePicker.findElement(By.xpath(".//td/button/span[(text()='" + month + "')]")).click();
+        datePicker.findElement(By.xpath(".//td/button/span[(text()='" + day + "')]/..")).click();
     }
 
     @Override //not use
@@ -359,6 +361,43 @@ public class MapPage implements IMapPage {
         }
     }
 
+    /**
+     * Find all problems except one filter by id (int)
+     * @param typeNumber is id for list (we find all problems expect this id 'typeNumber')
+     */
+    @Override  //not use
+    public void selectAllExceptOneFilter(int typeNumber) {
+        List<WebElement> filtersNames = driver.findElements(BY_PROBLEM_TYPE_WITH_LABEL_FOR);
+        List<WebElement> filtersChecks = driver.findElements(PROBLEM_TYPE_STARTS_WITH_TYPE_BY_ID);
+        for (int i = 0; i < filtersChecks.size(); i++) {
+            if (i != typeNumber - 1) {
+                if (!"true".equals(filtersChecks.get(i).getAttribute("checked"))) filtersNames.get(i).click();
+            } else {
+                if ("true".equals(filtersChecks.get(i).getAttribute("checked"))) filtersNames.get(i).click();
+            }
+        }
+    }
+
+    /**
+     * Find all problems except one filter by name (String) and click
+     * @param typeName we find all problems by filter expect this string 'typeName'
+     */
+    @Override  //not use
+    public void selectAllExceptOneFilter(String typeName) {
+        List<WebElement> filtersNames = driver.findElements(BY_PROBLEM_TYPE_WITH_LABEL_FOR);
+        List<WebElement> filtersChecks = driver.findElements(PROBLEM_TYPE_STARTS_WITH_TYPE_BY_ID);
+        for (int i = 0; i < filtersChecks.size(); i++) {
+            if (!typeName.equals(filtersNames.get(i).getText())) {
+                if ("true".equals(filtersChecks.get(i).getAttribute("checked"))) {
+                    filtersNames.get(i).click();
+                }
+            } else {
+                if (!"true".equals(filtersChecks.get(i).getAttribute("checked"))) {
+                    filtersNames.get(i).click();
+                }
+            }
+        }
+    }
 
 }
 
