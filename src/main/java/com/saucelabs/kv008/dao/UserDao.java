322f802420a98995a75736a9e0ae933a7b461b12
Den <denys.maxymchuk@gmail.com>
2015-09-03 11:22:46 +0300

    postgre db

--- src/main/java/com/saucelabs/kv008/dao/UserDao.java
+++ src/main/java/com/saucelabs/kv008/dao/UserDao.java
@@ -14,6 +14,7 @@ import java.util.List;
  * Created by dmakstc on 02.09.2015.
  */
 public class UserDao extends BaseDao<UserEntity, Integer> {
+
     @Override
     protected String getQuery(QueryType queryType) {
         String res;
