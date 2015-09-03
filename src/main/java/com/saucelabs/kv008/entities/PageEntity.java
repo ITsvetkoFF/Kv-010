cfae985cb8bc40fd4045deeacf26e61c842c415c
DenysMaksymchuk <denys.maxymchuk@gmail.com>
2015-09-04 00:57:14 +0300

    simple test

--- src/main/java/com/saucelabs/kv008/entities/PageEntity.java
+++ src/main/java/com/saucelabs/kv008/entities/PageEntity.java
@@ -16,4 +16,77 @@ public class PageEntity {
     private String title;
     private String content;
     private boolean isResource;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public boolean isResource() {
+        return isResource;
+    }
+
+    public void setResource(boolean isResource) {
+        this.isResource = isResource;
+    }
+
+    @Override
+    public String toString() {
+        return "PageEntity{" +
+                "id=" + id +
+                ", alias='" + alias + '\'' +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                ", isResource=" + isResource +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        PageEntity that = (PageEntity) o;
+
+        if (isResource != that.isResource) return false;
+        if (!alias.equals(that.alias)) return false;
+        if (!title.equals(that.title)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = alias.hashCode();
+        result = 31 * result + title.hashCode();
+        result = 31 * result + (isResource ? 1 : 0);
+        return result;
+    }
 }
